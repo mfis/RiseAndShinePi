74d9ee244ddb3236575b2369f15b907acc315e3c
mfis <fischermatthias1980@googlemail.com>
2015-05-04 18:02:06 +0200

    Renaming

--- ClockWorkPi/src/main/java/mfi/clockworkpi/logic/Constants.java
+++
@@ -1,7 +0,0 @@
-package mfi.clockworkpi.logic;
-
-public interface Constants {
-
-	final static long oneHourInMilliSeconds = 1000 * 60 * 60;
-
-}
--- ClockWorkPi/src/main/java/mfi/clockworkpi/main/CWPDevelopmentMode.java
+++
@@ -1,8 +0,0 @@
-package mfi.clockworkpi.main;
-
-public class CWPDevelopmentMode {
-
-	public static void main(String[] args) {
-		CWPMain.main(new String[] { CWPMain.DEVELOPMENT_MODE });
-	}
-}
